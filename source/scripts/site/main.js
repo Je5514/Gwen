cdcfc6a4ccfbb3a220970a1cbf43425710afc864
Je5514 <sreelakshminj@gmail.com>
2024-02-24 21:55:52 +0530

    change markup

--- source/scripts/site/main.js
+++ source/scripts/site/main.js
@@ -24,75 +24,18 @@ if((navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 0) || naviga
 // Site related functions
 
 //define a function 
-function example_function() { 
-    console.log("function called") 
-} 
-
-//call the function 
-example_function();
-
-//menu toggle
-window.addEventListener("DOMContentLoaded", (event) => {
-  const navToggle= document.querySelector('#Toggle');
-  const nav = document.querySelector('.menu-items');
-  navToggle.addEventListener("click",()=>{
-      nav.classList.toggle('menu-open')
-  });
-});
-
-window.addEventListener("DOMContentLoaded", (event) => {
-const navShop = document.querySelector('#shopbar');
-navShop.addEventListener("click", () => {
-    alert("You have no items in your shopping cart. Please add cart");
-});
-});
-
-//back to top button
-window.onscroll = function() {
-    scrollFunction();
-  };
-  
-  function scrollFunction() {
-    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
-    
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      scrollToTopBtn.style.display = "block";
-    } else {
-      scrollToTopBtn.style.display = "none";
-    }
-    // const blur = document.getElementById('video');
-    
-  }
-  
-  function scrollToTop() {
-     window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  }
-
-  //show password
-  function mypassword() {
-    var x = document.getElementById("password");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
-
-  $(document).ready(function() {
-    $(window).scroll(function() {
-      var scrollPosition = $(this).scrollTop();
-      var video = $('#background-video');
-  
-      if (scrollPosition > 0) {
-        video.addClass('video--blur');
+function HeaderSticky(){
+  if($('.header').length > 0){
+    window.addEventListener('scroll', function() {
+      if (window.scrollY > 1) {
+          $('.header').addClass('is-sticky');
       } else {
-        video.removeClass('video--blur');
+          $('.header').removeClass('is-sticky');
       }
     });
-  });
+  }
+}
+HeaderSticky();
 
 
   
